ff80e3f734c55d8874d9435171df0b15a5fb9106
Duncan <duncan@vtllf.org>
2016-11-17 23:30:01 +1300

    Fix unsorted keys in bench test data

--- json_tests/benches/bench_log.rs
+++ json_tests/benches/bench_log.rs
@@ -882,7 +882,7 @@ impl Write for MyMemWriter1 {
     }
 }
 
-const JSON_STR: &'static str = r#"{"timestamp":2837513946597,"zone_id":123456,"zone_plan":1,"http":{"protocol":2,"status":200,"host_status":503,"up_status":520,"method":1,"content_type":"text/html","user_agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36","referer":"https://www.cloudflare.com/","request_uri":"/cdn-cgi/trace"},"origin":{"ip":"1.2.3.4","port":8000,"hostname":"www.example.com","protocol":2},"country":238,"cache_status":3,"server_ip":"192.168.1.1","server_name":"metal.cloudflare.com","remote_ip":"10.1.2.3","bytes_dlv":123456,"ray_id":"10c73629cce30078-LAX"}"#;
+const JSON_STR: &'static str = r#"{"bytes_dlv":123456,"cache_status":3,"country":238,"http":{"content_type":"text/html","host_status":503,"method":1,"protocol":2,"referer":"https://www.cloudflare.com/","request_uri":"/cdn-cgi/trace","status":200,"up_status":520,"user_agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36"},"origin":{"hostname":"www.example.com","ip":"1.2.3.4","port":8000,"protocol":2},"ray_id":"10c73629cce30078-LAX","remote_ip":"10.1.2.3","server_ip":"192.168.1.1","server_name":"metal.cloudflare.com","timestamp":2837513946597,"zone_id":123456,"zone_plan":1}"#;
 
 #[test]
 fn test_encoder() {
